41e4a330586659b934120dcfd21e6e2b9c3ee701
Frann <pulpodesign@pm.me>
2026-07-30 21:05:28 +0200

    clean up and update submodule dependency

--- keyboards/kprepublic/cstc40/rev3/keymaps/arsenik_frann_mac/keymap.c
+++ keyboards/kprepublic/cstc40/rev3/keymaps/arsenik_frann_mac/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_vim_nav] = ARSENIK_LAYOUT(
         __,  G(KC_1),      G(KC_2),    G(KC_3),      G(KC_4),      G(KC_5),        G(KC_6),  G(KC_7),  G(KC_8),  G(KC_9),  G(KC_0),       __,
         __,  TG(_num_nav), C(AS(W)),   LSG(KC_TAB),  LGUI(KC_TAB), XX,             KC_HOME,  KC_PGDN,  KC_PGUP,  KC_END,   TG(_num_nav),  __,
-        __,  KC_WBAK,      KC_WFWD,    S(KC_TAB),    KC_TAB,       A(KC_I),        KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  XX,  __,
+        __,  G(KC_LEFT),   G(KC_RGHT), S(KC_TAB),    KC_TAB,       A(KC_I),        KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  XX,  __,
         __,  C(KC_PMNS),   C(KC_PPLS), C(S(KC_TAB)), C(KC_TAB),    G(KC_I),        MS_WHLL,  MS_WHLD,  MS_WHLU,  MS_WHLR,  XX,            __,
 
                                                                 __, KC_DEL, __, __, __
--- modules/frann/magic_layer_mod_tab/README.md
+++
@@ -1,3 +0,0 @@
-# Derivative work from J. Gandert’s Magic Layer Alt-Tab
-
-/!\ This is WIP based on [J. Gandert’s Magic Layer Alt-Tab](https://github.com/jgandert/qmk_modules). The intend is to generalize the feature on other modifier keys.
